80484f549b485e3380642def5420c9f05451222f
Arūnas Fortūnas <arunas.fortunas@gmail.com>
2019-10-20 15:55:15 +0300

    minor change

--- src/main/java/com/platform_lunar/homework/utils/AuthorizationUtils.java
+++ src/main/java/com/platform_lunar/homework/utils/AuthorizationUtils.java
@@ -5,7 +5,7 @@ import org.apache.tomcat.util.codec.binary.Base64;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
 public class AuthorizationUtils {
-    public static final String createEncodedAuthorization(String username, String password) {
+    public static String createEncodedAuthorization(String username, String password) {
         var auth = String.format("%s:%s", username, password);
         var encodedAuth = Base64.encodeBase64(auth.getBytes(US_ASCII));
         return String.format("Basic %s", new String(encodedAuth));
