ac463fd4e3c19196e4d4eb5638e5c45943c331a4
Arūnas Fortūnas <arunas.fortunas@gmail.com>
2019-10-22 00:06:32 +0300

    jackson config moved to application.yml

--- src/main/java/com/platform_lunar/homework/configurations/AppConfig.java
+++ src/main/java/com/platform_lunar/homework/configurations/AppConfig.java
@@ -1,23 +1,11 @@
 package com.platform_lunar.homework.configurations;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
-
 @Configuration
 public class AppConfig {
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
-        mapper.setSerializationInclusion(NON_NULL);
-        return mapper;
-    }
 
     @Bean
     public RestTemplate getRestTemplate() {
